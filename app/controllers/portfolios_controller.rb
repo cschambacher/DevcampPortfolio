199f00d8b35c48c045953bc4012c75d411bcebb3
corina <corina.schambacher@gmail.com>
2020-04-08 11:53:55 -0700

    edit and update

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -22,6 +22,24 @@ class PortfoliosController < ApplicationController
         end
     end
 
+    def edit
+        @portfolio_item = Portfolio.find(params[:id])
+    end
+
+    def update
+        @portfolio_item = Portfolio.find(params[:id])
+        
+        respond_to do |format|
+            if @portfolio_item.update(portfolio_params)
+                format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+                
+            else
+                format.html { render :edit }
+                
+            end
+        end
+    end
+
     private
 
     # Only allow a list of trusted parameters through.
