a91bd39a6df50c3d20dd607893dca666def2af40
corina <corina.schambacher@gmail.com>
2020-04-08 17:42:43 -0700

    show

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -28,7 +28,7 @@ class PortfoliosController < ApplicationController
 
     def update
         @portfolio_item = Portfolio.find(params[:id])
-        
+
         respond_to do |format|
             if @portfolio_item.update(portfolio_params)
                 format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
@@ -38,6 +38,10 @@ class PortfoliosController < ApplicationController
                 
             end
         end
+    end
+
+    def show
+        @portfolio_item = Portfolio.find(params[:id])
     end
 
     private
