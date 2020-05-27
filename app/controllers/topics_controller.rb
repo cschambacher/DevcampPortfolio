e1ec6c1450367cb75c78d0a867a069b656cbec17
corina <corina.schambacher@gmail.com>
2020-05-27 16:56:57 -0700

    implemented topics show with blogs foer the topic

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -6,5 +6,11 @@ class TopicsController < ApplicationController
 
   def show
     @topic = Topic.find(params[:id])
+
+    if logged_in?(:site_admin)
+      @blogs = @topic.blogs.recent.page(params[:page]).per(5)
+    else
+      @blogs = @topic.blogs.published.recent.page(params[:page]).per(5)
+    end
   end
 end
